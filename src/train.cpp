ca65b421ad4f85594c062b1b2a419b81811f716b
Dima2398 <159938169+Dima2398@users.noreply.github.com>
2024-04-26 12:05:50 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -32,45 +32,50 @@ int Train::getLength() {
 	int count_steps = 0;
 	int count_steps_2 = 0;
 	Cage* h = first;
-		if (h->light == false) {
-			h->light = true;
-			h = h->next;
-			count_steps++;
-			while (true) {
-				while (h->light != true) {
-					count_steps++;
-					h = h->next;
-				}
-				h = h->prev;
+	if (h->light == false) {
+		h->light = true;
+		h = h->next;
+		count_steps++;
+		while (true) {
+			while (h->light != true) {
+				count_steps++;
+				h = h->next;
+			}
+			h = h->prev;
+			count_steps_2++;
+			while (h->light != true) {
 				count_steps_2++;
-				while (h->light != true) {
-					count_steps_2++;
-					h = h->prev;
-				}
-				if (count_steps_2 == count_steps) {
-					countOp = count_steps * 2;
-					return count_steps;
-				}
-		    }
-	} else { //if (h->light == true) {
-			h->light = false;
-			h = h->next;
-			count_steps++;
-			while (true) {
-				while (h->light != false) {
-					count_steps++;
-					h = h->next;
-				}
 				h = h->prev;
-				count_steps_2++;
-				while (h->light != false) {
-					count_steps_2++;
-					h = h->prev;
+			}
+			if (count_steps_2 == count_steps) {
+				countOp = count_steps * 2;
+				return count_steps;
+			}
+		}
+	} else {
+		int count = 0;
+		int res = 0;
+		bool end = true;
+		Cage* cur = first;
+		do {
+			cur = cur->next;
+			count++;
+			res++;
+			if (cur->light) {
+				cur->light = false;
+				for (int i = count; i > 0; i--) {
+					cur = cur->prev;
+					res++;
 				}
-				if (count_steps_2 == count_steps) {
-					countOp = count_steps * 2;
-					return count_steps;
+				if (!cur->light) {
+					countOp = res;
+					return count;
+					end = false;
+				}
+				else {
+					count = 0;
 				}
 			}
+		} while (end);
 	}
 }
